e067a6a3818122e728acf74b0fd4ff99147f180c
Saron <saron@drcarmen.com>
2024-01-06 13:26:52 +0200

    Dynamic array data stucture

---
+++ src/main/java/data_structures_and_algorithms/dynamic_array/DynamicArray.java
@@ -0,0 +1,87 @@
+package data_structures_and_algorithms.dynamic_array;
+
+public class DynamicArray {
+
+    int size;
+    int capacity = 10;
+    Object[] array;
+
+    DynamicArray() {
+        this.array = new Object[capacity];
+    }
+
+    DynamicArray(int capacity) {
+        this.capacity = capacity;
+        this.array = new Object[capacity];
+    }
+
+    public void add(Object data) {
+        if (size == capacity)
+            grow();
+        array[size] = data;
+        size++;
+    }
+
+    public void insert(int index, Object data) {
+        if (size == capacity)
+            grow();
+        for (int i = size; i > index; i--)
+            array[i] = array[i - 1];
+        array[index] = data;
+        size++;
+    }
+
+    public void delete(Object data) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == data) {
+                for (int j = 0; j < size - i - 1; j++)
+                    array[i + j] = array[i + j + 1];
+                array[size - 1] = null;
+                size--;
+                if (size <= capacity / 3)
+                    shrink();
+                break;
+            }
+        }
+    }
+
+    public int search(Object data) {
+        for (int i = 0; i < capacity; i++)
+            if (array[i] == data)
+                return i;
+        return -1;
+    }
+
+    public void grow() {
+        int newCapacity = capacity * 2;
+        Object[] newArray = new Object[newCapacity];
+        if (capacity >= 0)
+            System.arraycopy(array, 0, newArray, 0, capacity);
+        this.array = newArray;
+        this.capacity = newCapacity;
+    }
+
+    public void shrink() {
+        int newCapacity = capacity / 2;
+        Object[] newArray = new Object[newCapacity];
+        if (capacity >= 0)
+            System.arraycopy(array, 0, newArray, 0, capacity);
+        this.array = newArray;
+        this.capacity = newCapacity;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder string = new StringBuilder();
+        for (int i = 0; i < capacity; i++)
+            string.append(array[i]).append(", ");
+        if (!string.toString().equals(""))
+            return "[" + string.substring(0, string.length() - 2) + "]";
+        return "[]";
+    }
+
+}
---
+++ src/main/java/data_structures_and_algorithms/dynamic_array/Main.java
@@ -0,0 +1,25 @@
+package data_structures_and_algorithms.dynamic_array;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DynamicArray dynamicArray = new DynamicArray(4);
+        dynamicArray.add("Saron");
+        dynamicArray.add("Patri");
+        System.out.println(dynamicArray);
+        System.out.println(dynamicArray.size);
+        dynamicArray.delete("Saron");
+        System.out.println(dynamicArray);
+        System.out.println(dynamicArray.size);
+        dynamicArray.add("Saron");
+        dynamicArray.insert(2, "Mati");
+        dynamicArray.add("Sami");
+        System.out.println(dynamicArray);
+        System.out.println(dynamicArray.size);
+        System.out.println("Found at index " + dynamicArray.search("Mati"));
+        dynamicArray.add("Andrei");
+        System.out.println(dynamicArray);
+        System.out.println(dynamicArray.capacity);
+
+    }
+}
