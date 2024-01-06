ff88858247f204a0e58851fbbe4fadaffbf83a17
Saron <saron@drcarmen.com>
2024-01-06 12:30:55 +0200

    LinkedList data structure

---
+++ src/main/java/data_structures_and_algorithms/linked_list/Main.java
@@ -0,0 +1,24 @@
+package data_structures_and_algorithms.linked_list;
+
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.offer("A");
+        linkedList.offer("B");
+        linkedList.offer("C");
+        linkedList.offer("D");
+        linkedList.offer("F");
+        linkedList.offer("G");
+        System.out.println(linkedList);
+        linkedList.removeLast();
+        System.out.println(linkedList);
+        linkedList.add(4, "E");
+        System.out.println(linkedList);
+        System.out.println(linkedList.peekFirst());
+        System.out.println(linkedList.peekLast());
+
+    }
+}
